9869f856f4498953822276f888a5f23f6b1bb8d2
jituanlin <jituanlin@tencent.com>
2025-04-29 22:19:02 +0800

    Update projectsData and add new images

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -12,38 +12,41 @@ type ProjectsData = {
 const projectsData: ProjectsData = {
   en: [
     {
-      title: 'A Search Engine',
-      description: `What if you could look up any information in the world? Webpages, images, videos
-        and more. Google has many features to help you find exactly what you're looking
-        for.`,
-      imgSrc: '/static/images/google.png',
-      href: 'https://www.google.com',
+      title: 'pipeable-es-fns',
+      description: `A typed, curried, ES6+ functions library for pipe style programming. 
+        Use it when you like pipe style programming, but Ramda is too cumbersome and 
+        fp-ts introduces too many FP concepts than you want. Just use native ES6+ 
+        functions with pipe style programming.`,
+      imgSrc: '/static/images/bulksplash-anton_lammert-LPPOau2BJNA.jpg',
+      href: 'https://github.com/jituanlin/pipeable-es-fns',
     },
     {
-      title: 'The Time Machine',
-      description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-        to the desired date and press "Go". No more worrying about lost keys or
-        forgotten headphones with this simple yet affordable solution.`,
-      imgSrc: '/static/images/time-machine.jpg',
-      href: '/blog/the-time-machine',
+      title: 'cookbook',
+      description: `A collection of code snippets organized by runtime. 
+        Currently includes Node.js snippets for fp-ts, monocle-ts, io-ts, rxjs, 
+        ramda and more. A practical resource for functional programming and 
+        reactive programming in TypeScript.`,
+      imgSrc: '/static/images/bulksplash-kirildobrev-v63UL8s28Ew.jpg',
+      href: 'https://github.com/jituanlin/cookbook',
     },
   ],
 
   zh: [
     {
-      title: '搜索引擎',
-      description: `如果您可以查找世界上的任何信息会怎样？网页、图片、视频
-        等等。谷歌提供了许多功能，帮助您找到您正在寻找的确切内容。`,
-      imgSrc: '/static/images/google.png',
-      href: 'https://www.google.com',
+      title: 'pipeable-es-fns',
+      description: `一个类型化、柯里化的ES6+函数库，用于管道式编程。
+        当您喜欢管道式编程，但Ramda太繁琐，而fp-ts引入了太多您不需要的FP概念时，
+        可以使用它。只需使用原生ES6+函数进行管道式编程。`,
+      imgSrc: '/static/images/bulksplash-anton_lammert-LPPOau2BJNA.jpg',
+      href: 'https://github.com/jituanlin/pipeable-es-fns',
     },
     {
-      title: '时间机器',
-      description: `想象一下能够穿越回过去或到未来。只需将旋钮转到
-        所需的日期并按下"开始"。有了这个简单而实惠的解决方案，
-        再也不用担心丢失钥匙或忘记耳机了。`,
-      imgSrc: '/static/images/time-machine.jpg',
-      href: '/blog/the-time-machine',
+      title: 'cookbook',
+      description: `代码片段集合，按运行时环境组织。
+        目前包含用于fp-ts、monocle-ts、io-ts、rxjs、ramda等的Node.js代码片段。
+        TypeScript中函数式编程和响应式编程的实用资源。`,
+      imgSrc: '/static/images/bulksplash-kirildobrev-v63UL8s28Ew.jpg',
+      href: 'https://github.com/jituanlin/cookbook',
     },
   ],
 }
